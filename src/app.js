ff464463e34caef76bbe529a498cfc9349082fba
liltine420 <cdon89@gmail.com>
2021-11-08 22:39:56 -0800

    temperature API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("city");
+  temperatureElement.innerHTML = Mat.round(response.data.main.temp);
+}
+
 let apiKey = "f64789992dbed6d446cc845a3728146a";
 let apiUrl =
-  "api.openweathermap.org/data/2.5/weather?id=New York&appid={apiKey}";
+  "api.openweathermap.org/data/2.5/weather?id=New York&appid={apiKey}&units=metric";
 
-console;
+axios.get(apiUrl).then(displayTemperature);
