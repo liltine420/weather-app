c5ab0104e94f9cd6346d4851f2b92dff467f0e58
liltine420 <cdon89@gmail.com>
2021-11-08 22:43:50 -0800

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("city");
+  let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Mat.round(response.data.main.temp);
+  cityElement.innerHTML = Mat.round(response.data.main.temp);
 }
 
 let apiKey = "f64789992dbed6d446cc845a3728146a";
 let apiUrl =
-  "api.openweathermap.org/data/2.5/weather?id=New York&appid={apiKey}&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric";
 
 axios.get(apiUrl).then(displayTemperature);
