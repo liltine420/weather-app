c00afa9c39a62848c49a0a376fd2e2e9a2bbf10b
liltine420 <cdon89@gmail.com>
2021-11-21 22:03:35 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "f64789992dbed6d446cc845a3728146a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}k&appid=${apiKey}&units=metric`;
+  let apiUrl =  `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -103,10 +103,10 @@ function displayTemperature(response) {
 }
 
 
-function search(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("city-input");
-  console.log(cityInputElement);
+function search(city) {
+  let apiKey = "f64789992dbed6d446cc845a3728146a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
@@ -115,24 +115,6 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFarenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  farenheitLink.classList.add("active");
-  let farenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(farenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  farenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
 
 let form = document.querySelector("search-form");
 form.addEventListener("submit", handleSubmit);
