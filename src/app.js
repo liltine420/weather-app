521514b55ec81bd4f6fec2265dbe8a7749561217
liltine420 <cdon89@gmail.com>
2021-11-28 10:04:54 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,6 +46,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let city = "Paris";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -84,5 +85,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("New York");
