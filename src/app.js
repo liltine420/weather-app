e125a87be27c722aa9998ebc0ded1763d3fa4dc1
liltine420 <cdon89@gmail.com>
2021-11-28 10:01:06 -0800

    code updated

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ function formatDate(timestamp) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
-  }
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
