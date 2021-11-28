a04c855bfdf33b1c220c2cb071d3a5b7fbcd5b10
liltine420 <cdon89@gmail.com>
2021-11-28 10:53:30 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -130,4 +130,11 @@ let celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 search("New York");
+displayForecast();
