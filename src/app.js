bd708ae88daa33ed1b7e76528f0d5f91529f984c
liltine420 <cdon89@gmail.com>
2021-11-28 19:27:36 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -96,6 +96,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "f64789992dbed6d446cc845a3728146a";
+  let city = "Seattle";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
