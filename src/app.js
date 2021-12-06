55bdaca4fa8d02398dabac65bf4f60d90b491cf2
liltine420 <cdon89@gmail.com>
2021-12-06 15:35:39 -0800

    fixed

--- src/app.js
+++ src/app.js
@@ -69,7 +69,6 @@ function getForecast(coordinates) {
   let apiKey = "f64789992dbed6d446cc845a3728146a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  displayForecast();
 }
 
 function displayWeatherCondition(response) {
