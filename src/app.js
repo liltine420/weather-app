b374121635bb9ece65204eaa4aede45629489324
liltine420 <cdon89@gmail.com>
2021-12-06 16:43:14 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
